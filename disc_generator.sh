2b4877c83e8b673f9ab2c7df49c30a9ac6212c1b
Javier Cejudo <javier@javiercejudo.com>
2016-02-19 08:18:49 +1100

    chore: add missing dep

---
+++ disc_generator.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p tmp
+
+BUNDLE=tmp/bundle.js
+BUNDLE_MIN=tmp/bundle.min.js
+DISC=tmp/disc.html
+DISC_MIN=tmp/disc.min.html
+NODE_BIN=node_modules/.bin
+
+browserify src/index.js --full-paths > $BUNDLE
+cat $BUNDLE | uglifyjs --mangle > $BUNDLE_MIN
+
+echo -e '\nO_o Generating discs'
+$NODE_BIN/discify $BUNDLE > $DISC
+$NODE_BIN/discify $BUNDLE_MIN > $DISC_MIN
